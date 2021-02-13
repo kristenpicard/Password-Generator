2d3f8a0ff1564cef89df9d4adf2757d78653f4d5
Kristen Picard <kristenpicard93@gmail.com>
2021-02-13 16:03:08 -0500

    Adding comments

--- Develop/script.js
+++ Develop/script.js
@@ -1,28 +1,34 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
 
+// This function takes in some number (n). Within it, we are creating an array with which our for loop with run through.
 function generateRandomNumbers(n) {
   var randomIntegersArray = [];
   for (i=0; i < n; i++) {
     randomIntegersArray.push(Math.floor(Math.random() * 10));
   }
+  // This returns our random group of numbers and converts them into a string and removes the commas between them.
   return randomIntegersArray.toString().replace(/,/g, '');
 }
 
+// This function returns a random lower case letter.
 function generateRandomLowercaseLetter() {
   var lowercaseAlphabet = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
-  var randomNumberBetween0an26 = Math.floor(Math.random() * 25);
-  return lowercaseAlphabet[randomNumberBetween0an26];
+  var randomNumberBetween0an25 = Math.floor(Math.random() * 25); // Generates a random number between 0 and 25.
+  return lowercaseAlphabet[randomNumberBetween0an25]; 
 }
 
+// This function returns a random upper case letter.
 function generateRandomUppercaseLetter() {
   var uppercaseAlphabet = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
-  var randomNumberBetween0an26 = Math.floor(Math.random() * 25);
-  return uppercaseAlphabet[randomNumberBetween0an26];
+  var randomNumberBetween0an25 = Math.floor(Math.random() * 25); // Generates a random number between 0 and 25.
+  return uppercaseAlphabet[randomNumberBetween0an25]; // This returns the character at the random index in the array.
 }
+
+// This function returns a random upper or lower case letter.
 function generateRandomUpperOrLowercaseLetter() {
   var randomNumberBetween0and9 = Math.floor(Math.random() * 10);
-  if (randomNumberBetween0and9 < 5) {
+  if (randomNumberBetween0and9 < 5) { 
     var generatedRandomLetter = generateRandomLowercaseLetter();
   }
   else { 
@@ -31,6 +37,7 @@ function generateRandomUpperOrLowercaseLetter() {
   return generatedRandomLetter;
 }
 
+// This function returns an array of random upper and lower case letters the size of n. 
 function generateRandomLetters(n) {
   var randomLettersArray = [];
   for (i=0; i < n; i++) {
@@ -41,6 +48,8 @@ function generateRandomLetters(n) {
 }
 
 
+
+// 
 function generatePassword() {
   var generatedPassword = generateRandomLetters(7);
   return generatedPassword;
