01954cd72dfa8b3bb30427bd9d3aa692af44f1b5
Kristen Picard <kristenpicard93@gmail.com>
2021-02-12 19:30:13 -0500

    Initial Progress

--- Develop/script.js
+++ Develop/script.js
@@ -1,6 +1,40 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
 
+function generateRandomNumbers(n) {
+  var randomIntegersArray = [];
+  for (i=0; i < n; i++) {
+    randomIntegersArray.push(Math.floor(Math.random() * 10));
+  }
+  return randomIntegersArray.toString().replace(/,/g, '');
+}
+
+function generateRandomLowercaseLetter() {
+  var lowercaseAlphabet = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
+  var randomNumberBetween0an26 = Math.floor(Math.random() * 25);
+  return lowercaseAlphabet[randomNumberBetween0an26];
+}
+
+function generateRandomUppercaseLetter() {
+  var uppercaseAlphabet = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
+  var randomNumberBetween0an26 = Math.floor(Math.random() * 25);
+  return uppercaseAlphabet[randomNumberBetween0an26];
+}
+
+function generateRandomLetters(n) {
+  var randomLettersArray = [];
+  for (i=0; i < n; i++) {
+    randomLettersArray.push(generateRandomLowercaseLetter());
+  }
+  return randomLettersArray.toString().replace(/,/g, '');
+}
+
+
+function generatePassword() {
+  var generatedPassword = generateRandomLetters(7);
+  return generatedPassword;
+}
+
 // Write password to the #password input
 function writePassword() {
   var password = generatePassword();
